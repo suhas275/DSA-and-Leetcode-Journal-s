2dcf0e741add35e853a6ed6bf521eec2421e3fcd
suhas <suhasgowdam275@gmail.com>
2025-05-28 10:08:48 +0530

    Buy and sell stock

--- Blink_75/ARRAY/TwoSumProblem.java
+++ Blink_75/ARRAY/TwoSumProblem.java
@@ -30,3 +30,16 @@ public class TwoSumProblem {
     }
 
 }
+
+
+// Output: This is the answer for variant 2: [1, 3]
+
+// Time Complexity: O(N), where N = size of the array.
+// Reason: The loop runs N times in the worst case and searching in a hashmap takes O(1) generally. So the time complexity is O(N).
+
+// Note: In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. In that case, the time complexity will be O(N2). If we use map instead of unordered_map, the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
+
+// Space Complexity: O(N) as we use the map data structure.
+
+
+// DETAILED EXPLANATION : https://takeuforward.org/data-structure/two-sum-check-if-a-pair-with-given-sum-exists-in-array/
