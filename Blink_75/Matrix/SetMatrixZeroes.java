dfa330941e7536b29c3de1d10817c3ffaf88a73a
suhas <suhasgowdam275@gmail.com>
2025-09-04 21:24:59 +0530

    73. Set Matrix Zeroes

---
+++ Blink_75/Matrix/SetMatrixZeroes.java
@@ -0,0 +1,89 @@
+package Blink_75.Matrix;
+
+public class SetMatrixZeroes {
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Step 1: Check first row
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Step 2: Check first column
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Step 3: Use first row/col as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Step 4: Update matrix based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 5: Update first row
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 6: Update first col
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    // Utility: Print Matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1,1,1},{1,0,1},{1,1,1}};
+        System.out.println("Original Matrix:");
+        printMatrix(matrix1);
+
+        setZeroes(matrix1);
+        System.out.println("After Setting Zeroes:");
+        printMatrix(matrix1);
+
+        int[][] matrix2 = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        System.out.println("\nOriginal Matrix:");
+        printMatrix(matrix2);
+
+        setZeroes(matrix2);
+        System.out.println("After Setting Zeroes:");
+        printMatrix(matrix2);
+    }
+}
